b78d25f92550279b27c68a3d898f012daf1bc798
xcode <xcode@stdlib.in>
2024-01-09 13:03:14 +0900

    1. update tag.sh

--- tag.sh
+++ tag.sh
@@ -5,24 +5,18 @@
 VERSION=$1
 COMMIT_ID=$(git rev-parse HEAD)
 COMMIT_ID=${COMMIT_ID:0:8}
-# BRANCH=$(git branch --show-current)
 
-ORIGIN=$1
+ORIGIN=$2
 if [ -z "$ORIGIN" ]; then
 	ORIGIN="origin"
 fi
 
 TAG="v$VERSION-$COMMIT_ID"
-LATEST="v$VERSION-latest"
 
 # delete tag
 git tag -d "$TAG"
-git tag -d "$LATEST"
 git push -d "$ORIGIN" "$TAG"
-git push -d "$ORIGIN" "$LATEST"
 
 # create tag
 git tag "$TAG"
-git tag "$LATEST"
 git push --tags "$ORIGIN" "$TAG"
-git push --tags "$ORIGIN" "$LATEST"
