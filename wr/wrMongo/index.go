1546242dbda381eab7082e82b28806455b895b09
xcode <xcode@stdlib.in>
2024-01-16 16:52:54 +0900

    1. add wrMongo.GetIndexList

--- wr/wrMongo/index.go
+++ wr/wrMongo/index.go
@@ -22,3 +22,42 @@ func IndexCollection(
 
 	return col.Indexes().CreateMany(ctx, idx)
 }
+
+/*------------------------------------------------------------------------------------------------*/
+
+type IndexResp struct {
+	Key    map[string]int64 `bson:"key"`
+	Name   string           `bson:"name"`
+	V      int64            `bson:"v"`
+	Unique *bool            `bson:"unique"`
+}
+
+type IndexRespList []*IndexResp
+
+func (x IndexRespList) StringList() (ls []string) {
+	ls = make([]string, len(x))
+	for i, resp := range x {
+		ls[i] = resp.Name
+	}
+	return
+}
+
+func GetIndexList(
+	ctx context.Context,
+	colNm string,
+) (ls IndexRespList, err error) {
+
+	var col = GetColP(ctx, colNm)
+
+	var cur *mongo.Cursor
+	if cur, err = col.Indexes().List(ctx); err != nil {
+		return
+	}
+
+	ls = make(IndexRespList, 0)
+	if err = cur.All(ctx, &ls); err != nil {
+		return
+	}
+
+	return
+}
